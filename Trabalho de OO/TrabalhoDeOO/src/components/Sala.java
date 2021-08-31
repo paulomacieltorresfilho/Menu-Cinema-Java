9ac9ec9a14530403a99abd003121ba3ceb2a148d
Nícolas Roberto <nicolas.roberto987@gmail.com>
2021-08-30 23:48:55 -0300

    Criação de classes

---
+++ Trabalho de OO/TrabalhoDeOO/src/components/Sala.java
@@ -0,0 +1,16 @@
+package components;
+
+import java.util.ArrayList;
+
+import components.Assento;
+
+public class Sala {
+	private ArrayList< Object > seats = new ArrayList< Object >();
+	private String type;
+	
+	public Sala() {
+		
+		}
+	
+	
+}
